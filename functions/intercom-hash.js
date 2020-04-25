cc4ba4656ad4a8cf99c84d8b7f88cb3be6e8b222
Steven Petryk <petryk.steven@gmail.com>
2020-04-25 14:40:03 -0700

    Test blog post

--- functions/intercom-hash.js
+++ functions/intercom-hash.js
@@ -3,12 +3,10 @@ const crypto = require("crypto")
 const { INTERCOM_IDENTITY_VERIFICATION_SECRET } = process.env
 
 exports.handler = function (event, context, callback) {
-  const { user } = context.clientContext
   const userHash = crypto
     .createHmac("sha256", INTERCOM_IDENTITY_VERIFICATION_SECRET)
-    .update(user.sub)
+    .update(context.clientContext.user.sub)
     .digest("hex")
-    .toString()
 
   callback(null, { statusCode: 200, body: userHash })
 }
